29eca581f4884a15eb64188d5484dd4568ca6ea5
daoketakx <3513442042@qq.com>
2024-12-21 23:13:40 +0800

    子模块提交信息

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -1,5 +1,6 @@
 #include "../exercise.h"
 
+unsigned long long area[90] = {0,1};
 constexpr unsigned long long fibonacci(int i) {
     switch (i) {
         case 0:
@@ -7,19 +8,19 @@ constexpr unsigned long long fibonacci(int i) {
         case 1:
             return 1;
         default:
-            return fibonacci(i - 1) + fibonacci(i - 2);
+            return area[i] ? area[i] : area[i] = fibonacci(i - 1) + fibonacci(i - 2);
     }
 }
 
 int main(int argc, char **argv) {
-    constexpr auto FIB20 = fibonacci(20);
+    const auto FIB20 = fibonacci(20);
     ASSERT(FIB20 == 6765, "fibonacci(20) should be 6765");
     std::cout << "fibonacci(20) = " << FIB20 << std::endl;
 
     // TODO: 观察错误信息，修改一处，使代码编译运行
     // PS: 编译运行，但是不一定能算出结果……
     constexpr auto ANS_N = 90;
-    constexpr auto ANS = fibonacci(ANS_N);
+    const auto ANS = fibonacci(ANS_N);
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
     return 0;
